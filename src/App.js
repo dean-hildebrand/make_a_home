a201cbf80e0b52d2f3950e61044dc96fcfb7c992
Dean Hildebrand <dean.hildebrand88@gmail.com>
2021-02-03 08:09:34 -0500

    Main image and style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Carousel from "./components/Carousel";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <Carousel />
     </div>
   );
 }
